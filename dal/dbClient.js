c18ab724aa224787802cc0c38b0ea01aecbf65dd
anyket <anyket@pop-os.localdomain>
2023-10-16 23:04:46 +0530

    using atlas mongo

--- dal/dbClient.js
+++ dal/dbClient.js
@@ -1,7 +1,7 @@
 const logger = require("logger");
 const { default: MongoDBClient } = require("nodemongodbclient");
 
-const dbCli = new MongoDBClient("docMate", ("mongodb://127.0.0.1:27017/?retryWrites=true&w=majority"));
+const dbCli = new MongoDBClient("docMate", ("mongodb+srv://aniket:Z6GjqBZ5mety0JXl@cluster0.rt82z3p.mongodb.net/?retryWrites=true&w=majority"));
 
 async function InitialiseMongoClient(){
   await dbCli.connect();
